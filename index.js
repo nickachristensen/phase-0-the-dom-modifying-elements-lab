ddf0a6e8729c4ea29741e0aeee262775239ba690
Nicholas A Christensen <nickachristensen@gmail.com>
2021-08-01 00:46:42 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+main.remove()
+
+const newHeader = document.createElement('h1');
+
+document.body.appendChild(newHeader);
+
+newHeader.setAttribute("id", "victory")
+
+newHeader.innerHTML = "NICK is the champion"
